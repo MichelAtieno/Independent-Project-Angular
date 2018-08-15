9833a5af137062a5fdd68ef9cccda893bfc38ed5
Michel Atieno <mishqamish@gmail.com>
2018-08-15 23:08:51 +0300

    Added custom date-pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: any): any {
-    let today:Date =new Date();
-  }
 
+  transform(value: any): number {
+    const today: Date = new Date();
+    const todayWithNoTime: any = new Date(today);
+    const dateDifference = todayWithNoTime - value;
+    const secondsInDay = 86400;
+
+    const dateDifferenceSeconds = dateDifference * 0.001;
+
+    const dateCounter = dateDifferenceSeconds / secondsInDay;
+    console.log(value);
+
+    if (dateCounter >= 1) {
+      return Math.round(dateCounter);
+     } else { return 0; }
+  }
+// tslint:disable-next-line:eofline
 }
